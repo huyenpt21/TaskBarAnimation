c58deb50173e582892f67f99d778bbd7205e88f9
huyenpt21 <huyenpthe140045@fpt.edu.vn>
2021-09-30 10:52:20 +0700

    first commit

---
+++ js/taskBar.js
@@ -0,0 +1,27 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+var tabs = $$('.tab-item')
+var panes = $$('.tab-pane')
+
+var tabActive = $('.tab-item.active');
+var line = $('.line');
+
+line.style.left = tabActive.offsetLeft + 'px';
+line.style.width = tabActive.offsetWidth + 'px';
+
+tabs.forEach(function (tab, index) {
+    var pane = panes[index];
+
+    tab.onclick = function () {
+        $('.tab-item.active').classList.remove('active');
+        $('.tab-pane.active').classList.remove('active');
+
+        this.classList.add('active');
+        pane.classList.add('active')
+
+        line.style.left = this.offsetLeft + 'px';
+        line.style.width = this.offsetWidth + 'px';
+
+    }
+})
